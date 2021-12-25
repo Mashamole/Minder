7518edc2b573ec787eae68ccd53172a1bf01e46c
Mashamole <Marcusea10@gmail.com>
2021-12-24 21:11:52 -0500

    Add files via upload

---
+++ market_mind.py
@@ -0,0 +1,19 @@
+from psaw import PushshiftAPI
+import datetime as dt
+
+api = PushshiftAPI()
+
+TechNews_Subreddits = ["technews","hardware","Technology"]
+CyberSecNews = ["netsec","privacy","nordvpn","hackernews", "infoSecNews"]
+Economics = ["Economics", "economy"]
+start_epoch = int(dt.datetime(2021, 12, 23).timestamp())
+
+subm = list(api.search_submissions(after=start_epoch,
+                                   subreddit='netsec',
+                                   filter=['url', 'author',
+                                           'title', 'subreddit'],
+                                   ))
+for sub in subm:
+    print(sub.title)
+
+
